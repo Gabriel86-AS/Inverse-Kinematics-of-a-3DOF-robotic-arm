b840291648a666babe9f9513b257790360520493
gebliar <gabrielanayasalas@gmail.com>
2026-03-08 10:53:08 -0500

    Upload 3DOF Inverse Kinematics

---
+++ .agent/rules/educational.md
@@ -0,0 +1,18 @@
+---
+trigger: always_on
+---
+
+You are my coding and robotics tutor. comprehensive inline comments explaining intent, design decisions, and implementation logic. 
+After the code block, provide a structured technical explanation covering:
+
+Problem breakdown
+
+Architectural approach
+
+Core algorithms and logic
+
+Tooling and dependencies used
+
+Trade-offs and possible optimizations
+
+Prioritize clarity, technical depth, and instructional value. Dont give me everything solved guide me  to a solution
--- InvKinematicsCalc.py
+++ InvKinematicsCalc.py
@@ -2,40 +2,77 @@
 ##import libaries
 import numpy as np
 import matplotlib.pyplot as pyplot
+
 ##Definir parametros (L1, L2, L3)
 L1 = 0
 L2 = 15
 L3 = 15
 ##Definir coordenadas del objetivo(x, y, z)
-x = 15
-y = 0
-z = 0
-##definir r k y q
-k = np.sqrt(x**2 + y**2) 
-q = z - L1
-r = np.sqrt(x**2 + y**2 + q**2)
-##valores por defecto de los angulos
-theta1 = 0
-theta2 = 0
-theta3 = 0
-##Verificar si objetivo esta dentro del alcance del robot
-if(r < L2 + L3):
+def invKinematicsCalc(x, y, z, Gr = True):
+
+     ##definir r k y q
+     k = np.sqrt(x**2 + y**2) 
+     q = z - L1
+     r = np.sqrt(x**2 + y**2 + q**2)
+     ##valores por defecto de los angulos
+     theta1 = 0
+     theta2 = 0
+     theta3 = 0
+     ##Verificar si objetivo esta dentro del alcance del robot
+     if(r < L2 + L3):
     
-     ## Calcular angulos
-     theta1 = np.atan2(y,x)
+          ## Calcular angulos
+          theta1 = np.atan2(y,x)
 
-     alpha = np.atan2(q,k)
-     beta = np.acos((L2**2 + r**2 - L3**2 )/(2*r*L2))
-     theta2 = alpha + beta
-     theta3 = np.pi -np.acos((L2**2 + L3**2 - r**2)/(2*L2*L3))
+          alpha = np.atan2(q,k)
+          beta = np.acos((L2**2 + r**2 - L3**2 )/(2*r*L2))
+          theta2 = alpha + beta
+          gamma = np.acos((L2**2 + L3**2 - r**2)/(2*L2*L3))
+          theta3 = np.pi - gamma
+          ##graficar la solucion con matplotlib si Gr == true
+        
+
+          theta1 = np.degrees(theta1)
+          theta2 = np.degrees(theta2)
+          theta3 = np.degrees(theta3)
+
+          ##checar limites fisicos
+
+          
+          #return theta1, theta2, theta3
+          print("theta1 = ", theta1)
+          print("theta2 = ", theta2)
+          print("theta3 = ", theta3)
+          return theta1, theta2, theta3
+     else:   
+         print("Objetivo fuera del alcance")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
      
-     ##convertir los angulos a grados de 0 a 180
+    
 
-     ##checar limites fisicos
-
-     ##graficar la solucion con matplotlib
-     #pasar al grados al robot    
-
-else:   
-    print("Objetivo fuera del alcance")
 
